99d1d2297dcb6c0e048530ed2a7d067297f414ff
Nana Ama Asantewaa Adu <136357748+nanaama100@users.noreply.github.com>
2024-04-07 22:38:05 +0000

    Add files via upload

---
+++ components3/data.js
@@ -0,0 +1,70 @@
+
+const list = [
+    {
+      id: 1,
+      title: "Tea with Salamatu bakes",
+      price: 50,
+      img: require('../asserts/Desert1.jpg'),
+      amount: 1,
+    },
+    {
+      id: 2,
+      title: "Bread and Toasted Eggs Only",
+      price: 45,
+      img: require('../asserts/Desert2.jpg'),
+      amount: 1,
+    },
+    {
+      id: 3,
+      title: "Hot Coffee Served with Ramadan Bread and Fruit",
+      price: 60,
+      img: require('../asserts/Desert3.jpg'),
+      amount: 1,
+    },
+    {
+      id: 4,
+      title: "TomBrown served with creamy milk",
+      price: 54.99,
+      img: require('../asserts/Desert4.jpg'),
+      amount: 1,
+    },
+    {
+      id: 5,
+      title: "Oatmeal and Grapes",
+      price: 49.99,
+      img: require('../asserts/Desert5.jpg'),
+      amount: 1,
+    },
+    {
+      id: 6,
+      title: "Oatmeal served with sliced banana toppings",
+      price: 45,
+      img: require('../asserts/Desert6.jpg'),
+      amount: 1,
+    },
+    {
+      id: 7,
+      title: "Fresh Milo with Janguar bread and fruits",
+      price: 50,
+      img: require('../asserts/Desert7.jpg'),
+      amount: 1,
+    },
+
+    {
+      id: 8,
+      title: "Brown Rice Porridge and sliced Banana toppings",
+      price: 50,
+      img: require('../asserts/Desert8.jpg'),
+      amount: 1,
+    },
+    {
+      id: 9,
+      title: "White Cornmeal with raspberry toppings ",
+      price: 55,
+      img: require('../asserts/Desert9.jpg'),
+      amount: 1,
+    },
+    
+];
+
+export default list;
